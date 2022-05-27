366dfb300603086bb7608563f27f674190495dc7
HermanSkop <german.skop@mail.ru>
2022-05-27 10:46:12 +0200

    warnings update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         JFrame main = new JFrame("Menu");
         JButton ng = new JButton("New Game");
         JButton hs = new JButton("High Scores");
@@ -18,29 +15,20 @@ public class Main {
         JButton hard = new JButton("Hard");
 
        //ExitGame exitGame = new ExitGame();
-        ez.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                difflvl.dispose();
-                main.setVisible(false);
-                new RunGame(1, main);
-            }
+        ez.addActionListener(e -> {
+            difflvl.dispose();
+            main.setVisible(false);
+            new RunGame(1, main);
         });
-        mid.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-               difflvl.dispose();
-                main.setVisible(false);
-               new RunGame(3, main);
-            }
+        mid.addActionListener(e -> {
+           difflvl.dispose();
+            main.setVisible(false);
+           new RunGame(3, main);
         });
-        hard.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                main.setVisible(false);
-                difflvl.dispose();
-                new RunGame(9, main);
-            }
+        hard.addActionListener(e -> {
+            main.setVisible(false);
+            difflvl.dispose();
+            new RunGame(9, main);
         });
 
         difflvl.add(ez, BorderLayout.LINE_START);
@@ -50,44 +38,28 @@ public class Main {
         difflvl.pack();
         difflvl.setLocationRelativeTo(null);
 
-        hs.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Container a = new GetFromFile().getCont();
-                ScoreListModel tempList;
-                try {
-                    tempList = new ScoreListModel(a.scoreList);
-                }
-                catch (NullPointerException c){
-                    a = new Container();
-                    tempList = new ScoreListModel(a.scoreList);
-                }
-                JList scoreList = new JList(tempList);
-                if(!scoreFrame[0].isVisible()) {
-                    JScrollPane tempScrollPane = new JScrollPane(scoreList);
-                    scoreFrame[0] = new JFrame("Score Table");
-                    scoreFrame[0].add(tempScrollPane);
-                    scoreFrame[0].setMinimumSize(new Dimension(150, 300));
-                    scoreFrame[0].pack();
-                    scoreFrame[0].setVisible(true);
-                }
+        hs.addActionListener(e -> {
+            Container a = new GetFromFile().getCont();
+            ScoreListModel tempList;
+            try {
+                tempList = new ScoreListModel(a.scoreList);
+            }
+            catch (NullPointerException c){
+                a = new Container();
+                tempList = new ScoreListModel(a.scoreList);
+            }
+            JList<ExitScore> scoreList = new JList<>(tempList);
+            if(!scoreFrame[0].isVisible()) {
+                JScrollPane tempScrollPane = new JScrollPane(scoreList);
+                scoreFrame[0] = new JFrame("Score Table");
+                scoreFrame[0].add(tempScrollPane);
+                scoreFrame[0].setMinimumSize(new Dimension(150, 300));
+                scoreFrame[0].pack();
+                scoreFrame[0].setVisible(true);
             }
         });
-        exit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(1);
-            }
-        });
-        ng.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (difflvl.isVisible()) {
-                    difflvl.setVisible(false);
-                }
-                else difflvl.setVisible(true);
-            }
-        });
+        exit.addActionListener(e -> System.exit(1));
+        ng.addActionListener(e -> difflvl.setVisible(!difflvl.isVisible()));
         main.add(ng, BorderLayout.LINE_START);
         main.add(hs, BorderLayout.CENTER);
         main.add(exit, BorderLayout.LINE_END);
